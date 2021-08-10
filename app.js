ac2f9b93ce6aaf027459d2f7b2bd4b55f6088e53
mzmormonde <zoeplacek@yahoo.com>
2021-08-09 22:51:34 -0500

    Change navbar to hamburger, fix button in navbar for theme change

--- app.js
+++ app.js
@@ -45,3 +45,20 @@ buttons.forEach(function (button) {
         }
     })
 })
+
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
